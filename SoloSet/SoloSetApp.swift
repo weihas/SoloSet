448e153715d4104183be93aee37799345b4fa077
weiha <weihas@icloud.com>
2021-06-11 10:50:00 +0800

    Initial Commit

---
+++ SoloSet/SoloSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloSetApp.swift
+//  SoloSet
+//
+//  Created by WeIHa'S on 2021/6/11.
+//
+
+import SwiftUI
+
+@main
+struct SoloSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
